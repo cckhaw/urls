077eb3e14a4d7fe74968e900a04b7620e1dc897b
Khaw Cheng Chuan <cckhaw@gmail.com>
2025-10-29 16:09:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import shutil
 
 def main():
     """Main Function"""
-    html = '<html><head><meta http-equiv="refresh" content="0;url={url}" /></head></html>'
+    html = '<html><title>~ khaw.cc ~</title><head><meta http-equiv="refresh" content="0;url={url}" /></head></html>'
 
     with open('links.json') as f:
         links = json.load(f)
