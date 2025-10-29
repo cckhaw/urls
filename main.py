eec2a987ed8b5867181a8271337b38ebe18fc1ac
Khaw Cheng Chuan <cckhaw@gmail.com>
2025-10-29 11:35:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     os.mkdir('dist')
 
     with open('dist/CNAME', 'w') as f:
-        f.write('khaw.cc')
+        f.write('cckhaw.github.io/urls')
 
     for link in links:
         html_document = html.format(url=link['url'])
